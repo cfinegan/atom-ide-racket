124113e37e6eae2a191fca9cabd9e6df0dd67c94
Conor Finegan <conor_finegan@student.uml.edu>
2018-01-24 15:41:11 -0500

    Server now starts correctly for racket files.
    This resolves an issue with the racket grammar, which uses a grammar scope of 'source.racket', not 'source.rkt'.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cp = require("child_process");
 
 class RacketLanguageClient extends AutoLanguageClient {
   getGrammarScopes() {
-    return ["source.rkt"];
+    return ["source.rkt", "source.racket"];
   }
   getLanguageName() {
     return "Racket";
@@ -15,15 +15,11 @@ class RacketLanguageClient extends AutoLanguageClient {
     const options = {};
     options.env = Object.create(process.env);
     options.env.ELECTRON_RUN_AS_NODE = "1";
-    options.env.ELECTRON_NO_ATTACH_CONSOLE = "1";
     const args = ["-t", "~/racket-langserver/main.rkt"];
     const child = cp.spawn("racket", args, options);
-    //this.captureServerErrors(child);
+    this.captureServerErrors(child);
     return child;
   }
-  // shouldStartForEditor(editor) {
-  //   return true;
-  // }
 }
 
 module.exports = new RacketLanguageClient();
