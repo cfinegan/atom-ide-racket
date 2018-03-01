a4b11ffdb26c519912a09536703e995fe1e4430e
Conor Finegan <conor_finegan@student.uml.edu>
2018-03-01 15:39:49 -0500

    Minor fix for Windows compatibility.
    auto-languageclient sets the ELECTRON_NO_ATTACH_CONSOLE env variable to 1; our override method should probably do the same.

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ class RacketLanguageClient extends AutoLanguageClient {
     const options = {};
     options.env = Object.create(process.env);
     options.env.ELECTRON_RUN_AS_NODE = "1";
+    options.env.ELECTRON_NO_ATTACH_CONSOLE = "1";
     const args = ["--lib", "racket-langserver"];
     const child = cp.spawn("racket", args, options);
     this.captureServerErrors(child);
